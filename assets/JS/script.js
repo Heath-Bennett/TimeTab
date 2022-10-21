eabfbc45db2faff77f506e9ccf48eb20049e1731
Heath Bennett <heathben418@gmail.com>
2022-10-21 17:12:51 -0400

    created slideshow background

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,6 +2,21 @@
 
 var allMonths = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 10000); // Change image every 10 seconds
+}
+
 const GetClock = () => {
     const d = new Date();
     const newMonth= d.getMonth();
@@ -10,15 +25,15 @@ const GetClock = () => {
     let newHour = d.getHours();
     let newMin= d.getMinutes();
     let meridiem = " ";
-
+    
     if (newYear<1000) {
         newYear += 1000;
     }
-
+    
     if (newMin <= 9){
         newMin = "0" + newMin;
     }
-
+    
     
     if (newHour >= 0 && newHour <= 11){
         meridiem = " AM"
